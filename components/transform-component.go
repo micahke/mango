16d1a0761fa31abab438e5643e06d361ea7b3eba
yumyumcurryman <micahkelias@gmail.com>
2023-05-08 23:02:03 -0400

    transform component ui

--- components/transform-component.go
+++ components/transform-component.go
@@ -1,86 +1,107 @@
 package components
 
 import (
+	"fmt"
+
 	"github.com/AllenDang/imgui-go"
+	// "github.com/micahke/mango/util"
 	"github.com/micahke/mango/util/math"
 )
 
 type TransformComponent struct {
-  Name string
+	Name     string
 	Position math.Vec3f
 	Rotation math.Vec3f
 	Scale    math.Vec3f
 }
-
 
 func (transform *TransformComponent) Init() {}
 
 // Don't really need much functionality from this every frame
 func (tranform *TransformComponent) Update() {}
 
+func (transform *TransformComponent) renderDragFloats(id string, first, second, third *float32) {
+
+	imgui.PushID(id)
+
+	imgui.ColumnsV(2, "", false)
+	imgui.SetColumnWidth(0, 100)
+	imgui.SetColumnWidth(1, 300)
+
+  var itemWidth float32 = (300 - (3 * 32.5)) / 3
+
+	imgui.Text(id)
+	imgui.NextColumn()
+
+  imgui.PushStyleVarVec2(imgui.StyleVarItemSpacing, imgui.Vec2{X: 0, Y:0})
+
+  imgui.PushStyleColor(imgui.StyleColorButton, imgui.Vec4{0.91, 0.30, 0.21, 1.0})
+  imgui.PushStyleColor(imgui.StyleColorButtonHovered, imgui.Vec4{1.0, 0.40, 0.31, 1.0})
+  imgui.PushStyleColor(imgui.StyleColorButtonActive, imgui.Vec4{1.0, 0.40, 0.31, 1.0})
+	if imgui.Button("X") {
+		fmt.Println("Okay")
+	}
+  imgui.PopStyleColorV(3)
+	imgui.SameLine()
+	imgui.PushItemWidth(itemWidth) // set a specific width for the DragFloat
+
+	imgui.DragFloatV("##X", first, 0.1, 0.0, 0.0, "%.3f", 1.0)
+
+	imgui.SameLine()
+
+  imgui.PushStyleColor(imgui.StyleColorButton, imgui.Vec4{0.15, 0.68, 0.38, 1.0})
+  imgui.PushStyleColor(imgui.StyleColorButtonHovered, imgui.Vec4{0.25, 0.78, 0.48, 1.0})
+  imgui.PushStyleColor(imgui.StyleColorButtonActive, imgui.Vec4{0.25, 0.78, 0.48, 1.0})
+	if imgui.Button("Y") {
+		fmt.Println("Okay")
+	}
+  imgui.PopStyleColorV(3)
+  imgui.PopItemWidth()
+	imgui.SameLine()
+	imgui.PushItemWidth(itemWidth) // set a specific width for the DragFloat
+	imgui.DragFloatV("##Y", second, 0.1, 0.0, 0.0, "%.3f", 1.0)
+  imgui.PopItemWidth()
+
+	imgui.SameLine()
+
+
+  imgui.PushStyleColor(imgui.StyleColorButton, imgui.Vec4{0.16, 0.50, 0.73, 1.0})
+  imgui.PushStyleColor(imgui.StyleColorButtonHovered, imgui.Vec4{0.26, 0.83, 0.83, 1.0})
+  imgui.PushStyleColor(imgui.StyleColorButtonActive, imgui.Vec4{0.26, 0.83, 0.83, 1.0})
+	if imgui.Button("Z") {
+		fmt.Println("Okay")
+	}
+  imgui.PopStyleColorV(3)
+	imgui.SameLine()
+	imgui.PushItemWidth(itemWidth) // set a specific width for the DragFloat
+	imgui.DragFloatV("##Z", third, 0.1, 0.0, 0.0, "%.3f", 1.0)
+  imgui.PopItemWidth()
+
+	imgui.PopStyleVar()
+
+	imgui.Columns()
+
+	imgui.PopID()
+}
 
 // Allows the component to designate its own control panel within the editor
 func (transform *TransformComponent) RenderControlPanel() {
+  imgui.Spacing()
 
-  
-  imgui.Text("Position")
+  transform.renderDragFloats("Translation", &transform.Position.X, &transform.Position.Y, &transform.Position.Z)
+  imgui.Spacing()
+  transform.renderDragFloats("Rotation", &transform.Rotation.X, &transform.Rotation.Y, &transform.Rotation.Z)
+  imgui.Spacing()
+  transform.renderDragFloats("Scale", &transform.Scale.X, &transform.Scale.Y, &transform.Scale.Z)
 
-  imgui.BeginChildV("Position", imgui.Vec2{X: 0, Y: 20}, false, 0)
-  imgui.SameLine()
-  imgui.PushItemWidth(75)
-  imgui.InputFloat("X", &transform.Position.X)
-
-  imgui.SameLine()
-  imgui.PushItemWidth(75)
-  imgui.InputFloat("Y", &transform.Position.Y)
-
-  imgui.SameLine()
-  imgui.PushItemWidth(75)
-  imgui.InputFloat("Z", &transform.Position.Z)
-
-  imgui.EndChild()
-
-  // Rotation
-
-  imgui.Text("Rotation")
-
-  imgui.BeginChildV("Rotation", imgui.Vec2{X: 0, Y: 20}, false, 0)
-  imgui.SameLine()
-  imgui.PushItemWidth(75)
-  imgui.InputFloat("X", &transform.Rotation.X)
-
-  imgui.SameLine()
-  imgui.PushItemWidth(75)
-  imgui.InputFloat("Y", &transform.Rotation.Y)
-
-  imgui.SameLine()
-  imgui.PushItemWidth(75)
-  imgui.InputFloat("Z", &transform.Rotation.Z)
-
-  imgui.EndChild()
-
-
-  imgui.Text("Scale")
-
-  imgui.BeginChildV("Scale", imgui.Vec2{X: 0, Y: 20}, false, 0)
-  imgui.SameLine()
-  imgui.PushItemWidth(75)
-  imgui.InputFloat("X", &transform.Scale.X)
-
-  imgui.SameLine()
-  imgui.PushItemWidth(75)
-  imgui.InputFloat("Y", &transform.Scale.Y)
-
-  imgui.SameLine()
-  imgui.PushItemWidth(75)
-  imgui.InputFloat("Z", &transform.Scale.Z)
-
-  imgui.EndChild()
-
-
+  imgui.Spacing()
+  imgui.Spacing()
 }
+
+
+
 
 // Keeping this here to avoid using reflection
 func (transform *TransformComponent) GetComponentName() string {
-  return transform.Name
+	return transform.Name
 }
